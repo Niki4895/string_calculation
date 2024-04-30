3c2b903a619324a869e9393702a979021dbdf7d6
Nikita Shah <nikitashah4895@gmail.com>
2024-04-30 18:15:12 +0530

    Format inputed string with array of string for process calculation

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
 class Calculator
+  attr_reader :arr_str
   def initialize(*str)
-    @str = str
+    @arr_str = str.join.split(", \"").map{|str| str.gsub("\"", "")}
   end
 end
--- spec/calculato_spec.rb
+++ spec/calculato_spec.rb
@@ -4,8 +4,10 @@ require_relative '../calculator'
 describe 'Calculator' do
   context "simple string calculation" do
     let(:str) { '"", "1", "1,5"' }
-    it "return addition" do
+    it "return formatted request params" do
       addition = Calculator.new(str)
+
+      expect(addition.arr_str).to eq(["", "1", "1,5"])
     end
   end
 end
